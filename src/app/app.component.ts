03aca42d5c1ca0b253c06662bd8a328f1c2b5ff5
iTzMiNOS <94097752+iTzMiNOS@users.noreply.github.com>
2023-06-18 23:11:06 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngFor';
+  num: number = 6;
+  postArray: Array<any> = [
+    {id:1,postTitle:'Post 1'},
+    {id:2,postTitle:'Post 2'},
+    {id:3,postTitle:'Post 3'},
+    {id:4,postTitle:'Post 4'},
+    {id:5,postTitle:'Post 5'}
+  ];
+  objArray: Array<any> = [
+    {id:1,postTitle:'Post 1'},
+    {id:2,postTitle:'Post 2'},
+    {id:3,postTitle:'Post 3'},
+    {id:4,postTitle:'Post 4'},
+    {id:5,postTitle:'Post 5'}
+  ];
+  addNew(){
+    this.objArray.push({id:this.num,postTitle:'Post ' + this.num})
+    this.num++;
+  }
+  onDelete(index:number){
+    this.objArray.splice(index,1);
+  }
 }
